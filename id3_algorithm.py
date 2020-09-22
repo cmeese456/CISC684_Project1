90ba2d1c5e6081ac8917b0b864d4ef33e0b3e4f0
techercise <mjl1997@verizon.net>
2020-09-22 10:10:46 -0400

    Commiting refactored code and working entropy and info. gain

--- id3_algorithm.py
+++ id3_algorithm.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 import math
 import copy
-import random
+# import random
 
 """This file is for implementing the ID3 decision tree learning algorithm. As discussed in class,
 the main step in decision tree learning is choosing the next attribute to split on. Implement the
@@ -13,11 +13,12 @@ following two heuristics for selecting the next attribute.
 2. Variance impurity heuristic (described below)"""
 
 # Below is some code to ensure the program is called with the correct arguments and to grab the input values
-#! It is commented for now until we need to use it and test it
+# It is commented for now until we need to use it and test it
 """
 # Ensure arguments were passed into the application
 if(len(sys.argv) != 6)
-    print 'Incorrect Usage! Please enter six arguments: <L>, <K>, <training-set>, <validation-set>, <test-set>, <to-print>'
+    print 'Incorrect Usage! Please enter six arguments: <L>, <K>, <training-set>, <validation-set>, <test-set>,
+     <to-print>'
     sys.exit(2)
 
 # Get command line arguments
@@ -29,7 +30,8 @@ test_set = sys.argv[4]
 to_print = sys.argv[5]
 """
 # Pandas Data Frame
-df = pd.read_csv('data_sets2/data_sets2/training_set.csv')
+# df = pd.read_csv('data_sets2/data_sets2/training_set.csv')
+
 
 # Evan's Node Class
 class Node:
@@ -54,14 +56,14 @@ class Node:
             sys.exit()
 
     def printout(self, node):
-        tree = ''
+        tree_ = ''
         if node:
-            tree += str(node.label)
-            if (self.left):
-                tree += self.printout(node.left)
-            if (self.right):
-                tree += self.printout(node.right)
-        return tree
+            tree_ += str(node.label)
+            if self.left:
+                tree_ += self.printout(node.left)
+            if self.right:
+                tree_ += self.printout(node.right)
+        return tree_
 
     # Commenting this because it caused an error since method is defined above
     # This is from Evan's fork
@@ -78,6 +80,7 @@ class Node:
 
         return str(self.label) """
 
+
 def printy(node):
     if node:
         print(node)
@@ -87,6 +90,7 @@ def printy(node):
             printy(node.right)
     return
 
+
 # Get the label (i.e. attribute name) with the highest information gain.
 #
 # In this version, only use a Pandas dataframe. Don't use Attribute objects or
@@ -95,7 +99,8 @@ def information_gain_heuristic(s, attr_list, df):
     gain_list = []
     for attr in attr_list:
         gain_list.append(information_gain(s, attr, df))
-    return max(gain_list, key=lambda item:item[1])[0]
+    return max(gain_list, key=lambda item: item[1])[0]
+
 
 # Get the information gain for an attribute.
 #
@@ -106,11 +111,21 @@ def information_gain(s, attr, df):
     s_pos = len(df.loc[(df[s] == 1)])
     s_neg = len(df.loc[(df[s] == 0)])
     s_total = len(df.index)
-    attr_val0_pos = len(df.loc[(df[s] == 1) & df[attr] == 0])
-    attr_val0_neg = len(df.loc[(df[s] == 0) & df[attr] == 0])
+    attr_val0_pos = 0
+    attr_val0_neg = 0
+    attr_val1_pos = 0
+    attr_val1_neg = 0
+    for i in range(len(df[s])):
+        if df[s][i] == 1 and df[attr][i] == 0:
+            attr_val0_pos += 1
+        elif df[s][i] == 0 and df[attr][i] == 0:
+            attr_val0_neg += 1
+        elif df[s][i] == 1 and df[attr][i] == 1:
+            attr_val1_pos += 1
+        elif df[s][i] == 0 and df[attr][i] == 1:
+            attr_val1_neg += 1
+
     attr_val0_total = len(df.loc[(df[attr] == 0)])
-    attr_val1_pos = len(df.loc[(df[s] == 1) & df[attr] == 1])
-    attr_val1_neg = len(df.loc[(df[s] == 0) & df[attr] == 1])
     attr_val1_total = len(df.loc[(df[attr] == 1)])
     attr_total = len(df.index)
     # The entropy of the entire set
@@ -119,18 +134,20 @@ def information_gain(s, attr, df):
     entropy_attr_0 = get_entropy(attr, attr_val0_pos, attr_val0_neg, attr_val0_total)
     # The entropy of the subset that has "1" as a value for the attribute
     entropy_attr_1 = get_entropy(attr, attr_val1_pos, attr_val1_neg, attr_val1_total)
-    # Subtract from the set's entropy the entropy of each value multiplied by its proporition in the set
-    gain = entropy_s[1] - (attr_val0_total/attr_total) * entropy_attr_0[1] - (attr_val1_total/attr_total) * entropy_attr_1[1]
+    # Subtract from the set's entropy the entropy of each value multiplied by its proportion in the set
+    gain = entropy_s[1] - (attr_val0_total / attr_total) * entropy_attr_0[1] - (attr_val1_total / attr_total) * entropy_attr_1[1]
     # Return the label of the attribute and its gain
     return attr, gain
+
 
 # Evan's get_entropy
 def get_entropy(node_label, val1_instances, val0_instances, total):
     """The formula for entropy is Entropy = -p_1 * log_2(p_1) - p_0 * log_2(p_0)"""
     """As per Mitchell p. 56, 0 log 0 is defined as zero """
     try:
-        entropy = (-1 * val1_instances/total * math.log2(val1_instances/total if val1_instances/total > 0 else 1)) - \
-                  (val0_instances/total * math.log2(val0_instances/total if val0_instances/total > 0 else 1))
+        entropy = (-1 * val1_instances / total * math.log2(
+            val1_instances / total if val1_instances / total > 0 else 1)) - \
+                  (val0_instances / total * math.log2(val0_instances / total if val0_instances / total > 0 else 1))
     # I'm getting an error where I have more attributes set to 0 or 1 than I
     # have total rows.
     except ZeroDivisionError as e:
@@ -142,12 +159,14 @@ def get_entropy(node_label, val1_instances, val0_instances, total):
         sys.exit()
     return node_label, entropy
 
+
 # This is the original get_entropy from Matt's commit
 def get_entropy_matt(node_label, positive_instances, negative_instances, total):
     """The formula for entropy is Entropy = -p_1 * log_2(p_1) - p_0 * log_2(p_0)"""
-    entropy = (-1 * positive_instances/total * math.log2(positive_instances/total)) - \
-              (negative_instances/total * math.log2(negative_instances/total))
+    entropy = (-1 * positive_instances / total * math.log2(positive_instances / total)) - \
+              (negative_instances / total * math.log2(negative_instances / total))
     return node_label, entropy
+
 
 def get_attribute_labels(dataset):
     column_names = dataset.columns
@@ -177,46 +196,47 @@ def get_attribute_labels(dataset):
 
     return attribute_labels_list, positive_instances_list, negative_instances_list, total_instances_list
 
+
 # examples_list     A pandas dataframe
 # target_attribute  The name of the target attribute (i.e. "Class")
 # attributes_list   A list of attribute names (i.e. "XC", "XD", etc.)
 def id3(examples_list, target_attribute, attributes_list):
     root = Node()
-    A = ''
+    # a = ''
     # If all examples are positive, set root's label to "1"
-    if (all(l == 1 for l in list(examples_list[target_attribute]))):
+    if all(labels == 1 for labels in list(examples_list[target_attribute])):
         root.label = 1
     # Elif all examples are negative, set root's label to "0"
-    elif (all(l == 0 for l in list(examples_list[target_attribute]))):
+    elif all(labels == 0 for labels in list(examples_list[target_attribute])):
         root.label = 0
-    # Elif attributes_list is empty, set root's label to most common value of
-    #   target attribute in examples
-    elif (not attributes_list):
+    # Elif attributes_list is empty, set root's label to most common value of target attribute in examples
+    elif not attributes_list:
         root.label = examples_list[target_attribute].mode()[0]
     else:
-        #A = random.choice(attributes_list) # For testing. Swap with info_gain
-        A = information_gain_heuristic(target_attribute, attributes_list, examples_list)
-        root.label = A
-        for i in [0,1]:
+        # A = random.choice(attributes_list) # For testing. Swap with info_gain
+        a = information_gain_heuristic(target_attribute, attributes_list, examples_list)
+        root.label = a
+        for i in [0, 1]:
             new_branch = Node()
             new_branch.label = str(i)
-            examples_list_vi = examples_list.loc[examples_list[A] == i]
-            if (examples_list_vi.empty):
+            examples_list_vi = examples_list.loc[examples_list[a] == i]
+            if examples_list_vi.empty:
                 new_leaf = Node()
                 new_leaf.label = examples_list[target_attribute].mode()[0]
                 new_branch.insert(new_leaf)
                 root.insert(new_branch)
             else:
                 trimmed_attributes = copy.deepcopy(attributes_list)
-                trimmed_attributes.remove(A)
-                new_branch.insert(id3(examples_list_vi,target_attribute,trimmed_attributes))
+                trimmed_attributes.remove(a)
+                new_branch.insert(id3(examples_list_vi, target_attribute, trimmed_attributes))
                 root.insert(new_branch)
     return root
 
-#! Note: Variance Impurity Heuristics are based off Evan's work on the Node and Attribute Classes
-#! They will only work once we merge this code with his side branch
+
+# Note: Variance Impurity Heuristics are based off Evan's work on the Node and Attribute Classes
+# They will only work once we merge this code with his side branch
 # This function calculates the variance impurity of all attributes in attr_dict in comparison to the total set s
-#! Important: attr_dict must only contain attributes to be tested, prune the input based on tree level before calling
+# Important: attr_dict must only contain attributes to be tested, prune the input based on tree level before calling
 def variance_impurity_heuristic(s, attr_dict):
     # Keep a list of the variance gain of each attribute
     gain_list = []
@@ -226,7 +246,8 @@ def variance_impurity_heuristic(s, attr_dict):
         gain_list.append(variance_impurity_gain(s, attr_dict[attr]))
 
     # Return the label of the attribute with the highest gain from the list
-    return max(gain_list, key=lambda item:item[1])[0]
+    return max(gain_list, key=lambda item: item[1])[0]
+
 
 def variance_impurity_gain(s, attr):
     # Get the variance impurity of the entire set
@@ -239,19 +260,21 @@ def variance_impurity_gain(s, attr):
     variance_attr_1 = variance_impurity(attr.label, attr.val1_pos, attr.val1_neg, attr.getTotalVal0())
 
     # Subtract the weighted variance impurity of each value from that of the total set
-    variance_gain = variance_s[1] - ((attr.getTotalVal0() / attr.getTotal()) * variance_attr_0[1] - (attr.getTotalVal1() / attr.getTotal()) * variance_attr_1[1])
+    variance_gain = variance_s[1] - (
+            (attr.getTotalVal0() / attr.getTotal()) * variance_attr_0[1] - (attr.getTotalVal1() / attr.getTotal()) *
+            variance_attr_1[1])
 
     # Return label and gain
     return attr.label, variance_gain
 
+
 # Variance Impurity = (K0/K)*(K1/K)
 def variance_impurity(node_label, val1_instances, val0_instances, total):
     # Calculate the variance impurity using the formula from the homework
-    variance_impurity = (val1_instances / total) * (val0_instances / total)
+    variance_impurity_ = (val1_instances / total) * (val0_instances / total)
 
     # Return the impurity and the label
-    return node_label, variance_impurity
-
+    return node_label, variance_impurity_
 
 
 def standard_output_format():
@@ -279,73 +302,73 @@ def standard_output_format():
 def print_tree():
     return ''
 
+
 # Computes the number of non-leaf
 # nodes in a tree.
-def countNonleaf(root):
-
+def count_n_on_leaf(root):
     # Base cases.
-    if (root == None or (root.left == None and
-                         root.right == None)):
+    if root is None or (root.left is None and root.right is None):
         return 0
 
     # If root is Not None and its one of
     # its child is also not None
-    return (1 + countNonleaf(root.left) +
-                countNonleaf(root.right))
+    return 1 + count_n_on_leaf(root.left) + count_n_on_leaf(root.right)
+
 
 # Implementation of the Post Pruning Algorithm
 # Input: An integer L and an integer K
 # Return: Post-pruned decision tree
-def post_pruning(L, K):
-    # Build a decision tree using all the training data
-    #? This will most likely need to be done using ID3 which is on Evan's Branch
-    #! Reminder to update this once we merge Evan's fork with our main codebase, commented for now
-    #decision_tree = id3(train_df, "Class", list(train_df.columns[0:-1]))
-    decision_tree = ""
+# def post_pruning(l, k):
+#     # Build a decision tree using all the training data
+#     # This will most likely need to be done using ID3 which is on Evan's Branch
+#     # Reminder to update this once we merge Evan's fork with our main codebase, commented for now
+#     # decision_tree = id3(train_df, "Class", list(train_df.columns[0:-1]))
+#     decision_tree = ""
+#
+#     # Optimal Decision Tree variable is initially set to the unpruned tree
+#     optimal_d = decision_tree
+#
+#     # Begin pruning with a Loop from 1 to L
+#     for x in [1, L]:
+#         # Copy the current most optimal tree into a new tree d_prime
+#         d_prime = optimal_d
+#
+#         # Select a random number M between 1 and K
+#         # random.randrange(start, stop, step)
+#         m = random.randrange(1, K, 1)
+#
+#         # Loop from 1 to m and start pruning subtrees
+#         for y in range(1, m):
+#             # Let n denote the number of non-leaf nodes in the decision tree d_prime
+#             n = count_n_on_leaf(d_prime)
+#
+#             # Order the nodes in d_prime from 1 to n
+#             # Reminder to implement
+#             # node_list = ""
+#
+#             # Select a random number between 1 and n, assign it to p
+#             p = random.randrange(1, n, 1)
+#
+#             # Replace the subtree rooted at P in d_prime with a leaf node.
+#             # Assign the leaf node the value of the majority class
+#             # Reminder to implement
+#             """ Not sure exactly how to implement this. Need a better understanding of Evan's Tree Code """
+#
+#         # End inner loop and evaluate the accuracy of d_prime on the validation set
+#         # First check the accuracy of our current optimal tree
+#         # Reminder to implement this
+#         optimal_d_accuracy = "Percent correctly classified examples"
+#
+#         # Calculate the accuracy of our newly pruned tree
+#         d_prime_accuracy = "Percent correctly classified examples"
+#
+#         # If d_prime_accuracy > optimal_d_accuracy, replace optimal_d with d_prime
+#         if d_prime_accuracy > optimal_d_accuracy:
+#             optimal_d = d_prime
+#
+#     # Return the pruned tree with the best accuracy
+#     return optimal_d
 
-    # Optimal Decision Tree variable is initially set to the unpruned tree
-    optimal_d = decision_tree
-
-    # Begin pruning with a Loop from 1 to L
-    for x in [1, L]:
-        # Copy the current most optimal tree into a new tree d_prime
-        d_prime = optimal_d
-
-        # Select a random number M between 1 and K
-        # random.randrange(start, stop, step)
-        m = random.randrange(1, K, 1)
-
-        # Loop from 1 to m and start pruning subtrees
-        for x in range(1, m):
-            # Let n denote the number of non-leaf nodes in the decision tree d_prime
-            n = countNonleaf(d_prime)
-
-            # Order the nodes in d_prime from 1 to n
-            #! Reminder to implement
-            node_list = ""
-
-            # Select a random number between 1 and n, assign it to p
-            p = random.randrange(1, n, 1)
-
-            # Replace the subtree rooted at P in d_prime with a leaf node.
-            # Assign the leaf node the value of the majority class
-            #! Reminder to implement
-            """ Not sure exactly how to implement this. Need a better understanding of Evan's Tree Code """
-
-        # End inner loop and evaluate the accuracy of d_prime on the validation set
-        # First check the accuracy of our current optimal tree
-        #! Reminder to implement this
-        optimal_d_accuracy = "Percent correctly classified examples"
-
-        # Calculate the accuracy of our newly pruned tree
-        d_prime_accuracy = "Percent correctly classified examples"
-
-        # If d_prime_accuracy > optimal_d_accuracy, replace optimal_d with d_prime
-        if (d_prime_accuracy > optimal_d_accuracy):
-            optimal_d = d_prime
-
-    # Return the pruned tree with the best accuracy
-    return optimal_d
 
 # l = 1
 # k = 0
@@ -361,9 +384,9 @@ train_df = pd.read_csv(training_set)
 validation_df = pd.read_csv(validation_set)
 test_df = pd.read_csv(test_set)
 
-micro_set = 'data_sets2/data_sets2/micro_set.csv'
-micro_df = pd.read_csv(micro_set)
+# micro_set = 'data_sets2/data_sets2/micro_set.csv'
+# micro_df = pd.read_csv(micro_set)
 
 tree = id3(train_df, "Class", list(train_df.columns[0:-1]))
-#tree = id3(micro_df, "Class", list(micro_df.columns[0:-1]))
+# tree = id3(micro_df, "Class", list(micro_df.columns[0:-1]))
 print(tree)
