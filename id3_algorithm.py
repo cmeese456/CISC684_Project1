1252a2c7f075e30e2c2484d012e078d53e42582c
techercise <mjl1997@verizon.net>
2020-09-24 09:55:18 -0400

    Test commit t otry and fix IDE issues

--- id3_algorithm.py
+++ id3_algorithm.py
@@ -29,8 +29,6 @@ validation_set = sys.argv[3]
 test_set = sys.argv[4]
 to_print = sys.argv[5]
 """
-# Pandas Data Frame
-# df = pd.read_csv('data_sets2/data_sets2/training_set.csv')
 
 
 # Evan's Node Class
@@ -46,11 +44,11 @@ class Node:
     def __str__(self):
         return str(self.label)
 
-    def insert(self, label):
+    def insert(self, node):
         if self.left is None:
-            self.left = Node(label)
+            self.left = node
         elif self.right is None:
-            self.right = Node(label)
+            self.right = node
         else:
             sys.stderr.write('Error: Cannot insert. All branches at node are set.\n')
             sys.exit()
@@ -92,109 +90,75 @@ def printy(node):
 
 
 # Get the label (i.e. attribute name) with the highest information gain.
-#
-# In this version, only use a Pandas dataframe. Don't use Attribute objects or
-# a dict container.
 def information_gain_heuristic(s, attr_list, df):
     gain_list = []
     for attr in attr_list:
-        gain_list.append(information_gain(s, attr, df))
+        gain_list.append(information_gain(s, attr, df))  # failing within information_gain
     return max(gain_list, key=lambda item: item[1])[0]
 
 
 # Get the information gain for an attribute.
-#
-# This version uses Pandas directly, without using my Attribute
-# class. In this version, s and attr are ONLY labels. Note that df (the Pandas
-# dataframe) is a new parameter.
 def information_gain(s, attr, df):
-    s_pos = len(df.loc[(df[s] == 1)])
-    s_neg = len(df.loc[(df[s] == 0)])
-    s_total = len(df.index)
+    s_pos = len(df.loc[(df[s] == 1)])   # Number of positive (i.e. Class=1) examples
+    s_neg = len(df.loc[(df[s] == 0)])   # Number of negative (i.e. Class=0) examples
+    s_total = len(df.index)             # Total number of examples
     attr_val0_pos = 0
     attr_val0_neg = 0
     attr_val1_pos = 0
     attr_val1_neg = 0
     for i in range(len(df[s])):
-        if df[s][i] == 1 and df[attr][i] == 0:
-            attr_val0_pos += 1
-        elif df[s][i] == 0 and df[attr][i] == 0:
-            attr_val0_neg += 1
-        elif df[s][i] == 1 and df[attr][i] == 1:
-            attr_val1_pos += 1
-        elif df[s][i] == 0 and df[attr][i] == 1:
-            attr_val1_neg += 1
-
+        try:
+            if df.iloc[i].loc[s] == 1 and df.iloc[i].loc[attr] == 0:
+                attr_val0_pos += 1
+            elif df.iloc[i].loc[s] == 0 and df.iloc[i].loc[attr] == 0:
+                attr_val0_neg += 1
+            elif df.iloc[i].loc[s] == 1 and df.iloc[i].loc[attr] == 1:
+                attr_val1_pos += 1
+            elif df.iloc[i].loc[s] == 0 and df.iloc[i].loc[attr] == 1:
+                attr_val1_neg += 1
+        except KeyError as e:
+            print(attr)
+            print(i)
+            print(df.iloc[i].loc[s])
+            # print(df.iloc[i].loc[attr])
+            print(df.iloc[i])
+            print(df)
+            sys.exit()
     attr_val0_total = len(df.loc[(df[attr] == 0)])
     attr_val1_total = len(df.loc[(df[attr] == 1)])
     attr_total = len(df.index)
     # The entropy of the entire set
     entropy_s = get_entropy(s, s_pos, s_neg, s_total)
     # The entropy of the subset that has "0" as a value for the attribute
-    entropy_attr_0 = get_entropy(attr, attr_val0_pos, attr_val0_neg, attr_val0_total)
+    try:
+        entropy_attr_0 = get_entropy(attr, attr_val0_pos, attr_val0_neg, attr_val0_total)
+    except ZeroDivisionError:
+        print(attr)
+        print(df)
+        sys.exit()
     # The entropy of the subset that has "1" as a value for the attribute
-    entropy_attr_1 = get_entropy(attr, attr_val1_pos, attr_val1_neg, attr_val1_total)
+    try:
+        entropy_attr_1 = get_entropy(attr, attr_val1_pos, attr_val1_neg, attr_val1_total)
+    except ZeroDivisionError:
+        print(attr)
+        print(df)
+        sys.exit()
     # Subtract from the set's entropy the entropy of each value multiplied by its proportion in the set
     gain = entropy_s[1] - (attr_val0_total / attr_total) * entropy_attr_0[1] - (attr_val1_total / attr_total) * entropy_attr_1[1]
     # Return the label of the attribute and its gain
     return attr, gain
 
 
-# Evan's get_entropy
 def get_entropy(node_label, val1_instances, val0_instances, total):
     """The formula for entropy is Entropy = -p_1 * log_2(p_1) - p_0 * log_2(p_0)"""
     """As per Mitchell p. 56, 0 log 0 is defined as zero """
-    try:
+    if total == 0:
+        entropy = 0
+    else:
         entropy = (-1 * val1_instances / total * math.log2(
             val1_instances / total if val1_instances / total > 0 else 1)) - \
                   (val0_instances / total * math.log2(val0_instances / total if val0_instances / total > 0 else 1))
-    # I'm getting an error where I have more attributes set to 0 or 1 than I
-    # have total rows.
-    except ZeroDivisionError as e:
-        print("val1_instances: " + str(val1_instances))
-        print("val0_instances: " + str(val0_instances))
-        print("total: " + str(total))
-        print("node: " + node_label)
-        print(e)
-        sys.exit()
     return node_label, entropy
-
-
-# This is the original get_entropy from Matt's commit
-def get_entropy_matt(node_label, positive_instances, negative_instances, total):
-    """The formula for entropy is Entropy = -p_1 * log_2(p_1) - p_0 * log_2(p_0)"""
-    entropy = (-1 * positive_instances / total * math.log2(positive_instances / total)) - \
-              (negative_instances / total * math.log2(negative_instances / total))
-    return node_label, entropy
-
-
-def get_attribute_labels(dataset):
-    column_names = dataset.columns
-
-    attribute_labels_list = []
-    positive_instances_list = []
-    negative_instances_list = []
-    total_instances_list = []
-    for col in column_names:
-        positive_instances = 0
-        negative_instances = 0
-        current_column = dataset[col]
-        for i in range(len(current_column)):
-            if current_column[i] == 1:
-                positive_instances += 1
-            elif current_column[i] == 0:
-                negative_instances += 1
-            else:
-                sys.stderr.write('ILLEGAL VALUE FOUND IN CLASS DATA')
-                sys.exit()
-        total = positive_instances + negative_instances
-        # for each column, append the column label, the number of 1s, the number of 0s, and the total number of values
-        attribute_labels_list.append(col)
-        positive_instances_list.append(positive_instances)
-        negative_instances_list.append(negative_instances)
-        total_instances_list.append(total)
-
-    return attribute_labels_list, positive_instances_list, negative_instances_list, total_instances_list
 
 
 # examples_list     A pandas dataframe
@@ -202,24 +166,25 @@ def get_attribute_labels(dataset):
 # attributes_list   A list of attribute names (i.e. "XC", "XD", etc.)
 def id3(examples_list, target_attribute, attributes_list):
     root = Node()
-    # a = ''
+    A = ''
     # If all examples are positive, set root's label to "1"
-    if all(labels == 1 for labels in list(examples_list[target_attribute])):
+    if all(l == 1 for l in list(examples_list[target_attribute])):
         root.label = 1
     # Elif all examples are negative, set root's label to "0"
-    elif all(labels == 0 for labels in list(examples_list[target_attribute])):
+    elif all(l == 0 for l in list(examples_list[target_attribute])):
         root.label = 0
-    # Elif attributes_list is empty, set root's label to most common value of target attribute in examples
+    # Elif attributes_list is empty, set root's label to most common value of
+    #   target attribute in examples
     elif not attributes_list:
         root.label = examples_list[target_attribute].mode()[0]
     else:
         # A = random.choice(attributes_list) # For testing. Swap with info_gain
-        a = information_gain_heuristic(target_attribute, attributes_list, examples_list)
-        root.label = a
+        A = information_gain_heuristic(target_attribute, attributes_list, examples_list)
+        root.label = A
         for i in [0, 1]:
             new_branch = Node()
             new_branch.label = str(i)
-            examples_list_vi = examples_list.loc[examples_list[a] == i]
+            examples_list_vi = examples_list.loc[examples_list[A] == i]
             if examples_list_vi.empty:
                 new_leaf = Node()
                 new_leaf.label = examples_list[target_attribute].mode()[0]
@@ -227,7 +192,7 @@ def id3(examples_list, target_attribute, attributes_list):
                 root.insert(new_branch)
             else:
                 trimmed_attributes = copy.deepcopy(attributes_list)
-                trimmed_attributes.remove(a)
+                trimmed_attributes.remove(A)
                 new_branch.insert(id3(examples_list_vi, target_attribute, trimmed_attributes))
                 root.insert(new_branch)
     return root
@@ -260,10 +225,8 @@ def variance_impurity_gain(s, attr):
     variance_attr_1 = variance_impurity(attr.label, attr.val1_pos, attr.val1_neg, attr.getTotalVal0())
 
     # Subtract the weighted variance impurity of each value from that of the total set
-    variance_gain = variance_s[1] - (
-            (attr.getTotalVal0() / attr.getTotal()) * variance_attr_0[1] - (attr.getTotalVal1() / attr.getTotal()) *
-            variance_attr_1[1])
-
+    variance_gain = variance_s[1] - ((attr.getTotalVal0() / attr.getTotal()) * variance_attr_0[1] -
+                                     (attr.getTotalVal1() / attr.getTotal()) * variance_attr_1[1])
     # Return label and gain
     return attr.label, variance_gain
 
@@ -277,7 +240,7 @@ def variance_impurity(node_label, val1_instances, val0_instances, total):
     return node_label, variance_impurity_
 
 
-def standard_output_format():
+def standard_output_format(dt):
     """This function prints the decision tree to standard output. An example of standard output is below.
     wesley = 0 :
     | honor = 0 :
@@ -290,7 +253,57 @@ def standard_output_format():
     According to this tree, if wesley = 0, honor = 0, and barclay = 0, then the class value of the corresponding
     instance should be 1. In other words, the value appearing before a colon is an attribute value, and the value
     appearing after a colon is a class value."""
-    return ''
+    tree_level = 0
+    left_node = 0
+    right_node = 0
+    while left_node is not None and right_node is not None:
+        if tree_level == 0:
+            if left_node is not None:
+                left_node = dt.left
+                print(dt.label, '=', left_node.label, ':')
+            elif right_node is not None:
+                right_node = dt.right
+                print(dt.label, '=', right_node.label, ':')
+        elif tree_level > 0:
+            while left_node.left is not None:
+                to_print = ''
+                for i in range(tree_level):
+                    to_print += '| '
+                left_node = left_node.left
+                left_branch_value = left_node.left
+                left_branch_value = left_branch_value.label
+                if type(left_branch_value) != str:
+                    str(left_branch_value)
+                to_print += left_node.label + ' = ' + left_branch_value + ' : '
+                print(to_print)
+                tree_level += 1
+        tree_level += 1
+
+        # if tree_level > 0:
+        #     for i in range(tree_level):
+        #         visual_level += '| '
+        #     if left_node is not None:
+        #         left_node = left_node.left
+        #         left_node = left_node.label
+        #         visual_level += left_node.label
+        #
+        #         print(visual_level, '=',  ':', )
+        #     elif right_node is not None:
+        #         right_node = right_node.right
+        #         visual_level += right_node.label
+        # if tree_level == 0:  # needed to format tree to project specifications
+        #     # print(dt.label, '= ', VALUE, ': ', VALUE)
+        #     if dt.left is not None:
+        #         left_node = dt.left
+        #         left_node = left_node.label  # each branch has a new node set up in the label field
+        #         branch_label = left_node.label  # call label again to get the label of the branch, 0 or 1
+        #         print(dt.label, '=', branch_label, ': ')
+        #     elif dt.right is not None:
+        #         right_node = dt.right
+        #         right_node = right_node.label
+        #         branch_label = right_node.label
+        #         print(dt.label, '=', branch_label, ': ')
+        # tree_level += 1
 
 
 # class_label, pos, neg, tot = get_class_labels(df)
@@ -299,20 +312,19 @@ def standard_output_format():
 # print(get_attribute_labels(df))
 
 
-def print_tree():
-    return ''
+# def print_tree(tree):
+#     return ''
 
 
 # Computes the number of non-leaf
 # nodes in a tree.
-def count_n_on_leaf(root):
+def count_non_leaf(root):
     # Base cases.
     if root is None or (root.left is None and root.right is None):
         return 0
 
-    # If root is Not None and its one of
-    # its child is also not None
-    return 1 + count_n_on_leaf(root.left) + count_n_on_leaf(root.right)
+    # If root is Not None and its one of its child is also not None
+    return 1 + count_non_leaf(root.left) + count_non_leaf(root.right)
 
 
 # Implementation of the Post Pruning Algorithm
@@ -372,7 +384,8 @@ def count_n_on_leaf(root):
 
 # l = 1
 # k = 0
-training_set = 'data_sets2/data_sets2/training_set.csv'
+# training_set = 'data_sets2/data_sets2/training_set.csv'
+training_set = 'data_sets1/data_sets1/example_set.csv'
 validation_set = 'data_sets2/data_sets2/validation_set.csv'
 test_set = 'data_sets2/data_sets2/test_set.csv'
 to_print = 'yes'
@@ -387,6 +400,6 @@ test_df = pd.read_csv(test_set)
 # micro_set = 'data_sets2/data_sets2/micro_set.csv'
 # micro_df = pd.read_csv(micro_set)
 
-tree = id3(train_df, "Class", list(train_df.columns[0:-1]))
+tree = id3(train_df, "PlayTennis", list(train_df.columns[0:-1]))
 # tree = id3(micro_df, "Class", list(micro_df.columns[0:-1]))
-print(tree)
+standard_output_format(tree)
