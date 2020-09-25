25cddfb6af3d5084cfcd63e66a9ab4c918283e1b
cmeese456 <collin.meese@gmail.com>
2020-09-25 13:53:07 -0400

    Variance Impurity Heuristics updated

--- id3_algorithm.py
+++ id3_algorithm.py
@@ -191,36 +191,77 @@ def id3(examples_list, target_attribute, attributes_list):
 # ! They will only work once we merge this code with his side branch
 # This function calculates the variance impurity of all attributes in attr_dict in comparison to the total set s
 # ! Important: attr_dict must only contain attributes to be tested, prune the input based on tree level before calling
-def variance_impurity_heuristic(s, attr_dict):
+def variance_impurity_heuristic(s, attr_list, df):
     # Keep a list of the variance gain of each attribute
     gain_list = []
 
     # Calculate the variance impurity gain of each attribute in the list
-    for attr in attr_dict:
-        gain_list.append(variance_impurity_gain(s, attr_dict[attr]))
+    for attr in attr_list:
+        gain_list.append(variance_impurity_gain(s, attr, df))
 
     # Return the label of the attribute with the highest gain from the list
     return max(gain_list, key=lambda item: item[1])[0]
 
 
-def variance_impurity_gain(s, attr):
-    # Get the variance impurity of the entire set
-    variance_s = variance_impurity(s.label, s.getTotalVal1(), s.getTotalVal0(), s.getTotal())
+def variance_impurity_gain(s, attr, df):
+    s_pos = len(df.loc[(df[s] == 1)])  # Number of positive (i.e. Class=1) examples
+    s_neg = len(df.loc[(df[s] == 0)])  # Number of negative (i.e. Class=0) examples
+    s_total = len(df.index)  # Total number of examples
+    attr_val0_pos = 0
+    attr_val0_neg = 0
+    attr_val1_pos = 0
+    attr_val1_neg = 0
+    for i in range(len(df[s])):
+        try:
+            # if df[s][i] == 1 and df[attr][i] == 0:
+            if df.iloc[i].loc[s] == 1 and df.iloc[i].loc[attr] == 0:
+                attr_val0_pos += 1
+            # elif df[s][i] == 0 and df[attr][i] == 0:
+            elif df.iloc[i].loc[s] == 0 and df.iloc[i].loc[attr] == 0:
+                attr_val0_neg += 1
+            # elif df[s][i] == 1 and df[attr][i] == 1:
+            elif df.iloc[i].loc[s] == 1 and df.iloc[i].loc[attr] == 1:
+                attr_val1_pos += 1
+            # elif df[s][i] == 0 and df[attr][i] == 1:
+            elif df.iloc[i].loc[s] == 0 and df.iloc[i].loc[attr] == 1:
+                attr_val1_neg += 1
+        except KeyError as e:
+            print(attr)
+            print(i)
+            print(df.iloc[i].loc[s])
+            # print(df.iloc[i].loc[attr])
+            print(df.iloc[i])
+            print(df)
+            sys.exit()
+    attr_val0_total = len(df.loc[(df[attr] == 0)])
+    attr_val1_total = len(df.loc[(df[attr] == 1)])
+    attr_total = len(df.index)
 
-    # Get the variance impurity of the subset that has "0" as a value for the attribute
-    variance_attr_0 = variance_impurity(attr.label, attr.val0_pos, attr.val0_neg, attr.getTotalVal0())
+    # Calculate the Variance Impurity of the entire set
+    variance_s = variance_impurity(s, s_pos, s_neg, s_total)
 
-    # Get the variance impurity of the subset that has "1" as the value for the attribute
-    variance_attr_1 = variance_impurity(attr.label, attr.val1_pos, attr.val1_neg, attr.getTotalVal0())
+    # Calculate the Variance Impurtiy of the subset that has "0" as the attribute value
+    try:
+        variance_attr_0 = variance_impurity(attr, attr_val0_pos, attr_val0_neg, attr_val0_total)
+    except ZeroDivisionError:
+        print(attr)
+        print(df)
+        sys.exit()
 
-    # Subtract the weighted variance impurity of each value from that of the total set
-    variance_gain = variance_s[1] - (
-                (attr.getTotalVal0() / attr.getTotal()) * variance_attr_0[1] - (attr.getTotalVal1() / attr.getTotal()) *
-                variance_attr_1[1])
+    # Calculate the Variance Impurity of the subset that has "1" as the attribute value
+    try:
+        variance_attr_1 = variance_impurity(attr, attr_val1_pos, attr_val1_neg, attr_val1_total)
+    except ZeroDivisionError:
+        print(attr)
+        print(df)
+        sys.exit()
 
-    # Return label and gain
-    return attr.label, variance_gain
-
+    # Subtract from the set's variance the variance of each value multiplied by its proportion in the set
+    gain = variance_s[1] - (attr_val0_total / attr_total) * variance_attr_0[1] - (attr_val1_total / attr_total) * \
+        variance_attr_1[1]
+    
+    # Return the label of the attribute and its gain
+    return attr, gain
 
 # Variance Impurity = (K0/K)*(K1/K)
 def variance_impurity(node_label, val1_instances, val0_instances, total):
@@ -229,6 +270,67 @@ def variance_impurity(node_label, val1_instances, val0_instances, total):
 
     # Return the impurity and the label
     return node_label, variance_impurity
+
+
+# examples_list     A pandas dataframe
+# target_attribute  The name of the target attribute (i.e. "Class")
+# attributes_list   A list of attribute names (i.e. "XC", "XD", etc.)
+def id3_variance_impurity(examples_list, target_attribute, attributes_list):
+    root = Node()
+    A = ''
+    # If all examples are positive, set root's label to "1"
+    if all(l == 1 for l in list(examples_list[target_attribute])):
+        root.label = 1
+    # Elif all examples are negative, set root's label to "0"
+    elif all(l == 0 for l in list(examples_list[target_attribute])):
+        root.label = 0
+    # Elif attributes_list is empty, set root's label to most common value of
+    #   target attribute in examples
+    elif not attributes_list:
+        root.label = examples_list[target_attribute].mode()[0]
+    else:
+        # A = random.choice(attributes_list) # For testing. Swap with info_gain
+        A = variance_impurity_heuristic(target_attribute, attributes_list, examples_list)
+        root.label = A
+        for i in [0, 1]:
+            # The code below creates a node in-between a parent node and its
+            # child. The sole purpose of this in-between node is to bear a label
+            # reflecting whether the parent node's value was 0 or 1 for the
+            # child branch. BUT that information is already conveyed by the
+            # order in which nodes are inserted. The first insertion goes to the
+            # left and signifies a "0" value for the parent node. The second
+            # goes on the right and signifies a "1" value for the parent node.
+            # So this in-between node is redundant.
+            new_branch = Node()
+            new_branch.label = str(i)
+            examples_list_vi = examples_list.loc[examples_list[A] == i]
+            if examples_list_vi.empty:
+                new_leaf = Node()
+                new_leaf.label = examples_list[target_attribute].mode()[0]
+                new_branch.insert(new_leaf)
+                root.insert(new_branch)
+            else:
+                trimmed_attributes = copy.deepcopy(attributes_list)
+                trimmed_attributes.remove(A)
+                new_branch.insert(id3(examples_list_vi, target_attribute, trimmed_attributes))
+                root.insert(new_branch)
+            # This code does away with separate "in-between" nodes. The
+            # attribute value that leads from a parent to its child is
+            # represented by which side the child is inserted on. Left nodes
+            # (inserted first) represent a "0" value. Right nodes represent
+            # a "1" value.
+            '''
+            examples_list_vi = examples_list.loc[examples_list[A] == i]
+            if (examples_list_vi.empty):
+                new_leaf = Node()
+                new_leaf.label = examples_list[target_attribute].mode()[0]
+                root.insert(new_leaf)
+            else:
+                trimmed_attributes = copy.deepcopy(attributes_list)
+                trimmed_attributes.remove(A)
+                root.insert(id3(examples_list_vi,target_attribute,trimmed_attributes))
+                '''
+    return root
 
 
 # Computes the number of non-leaf nodes in a tree.
